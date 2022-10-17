d612d7176b0060dfc5c7fdd004031e3a467f31af
StephenSunday <sundaystephen864@gmail.com>
2022-10-17 15:12:53 -0700

    bash

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%i", n);
+
+	printf("\n");
+	return (0);
+}
